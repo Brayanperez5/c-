07bf484c428e752807876a9de085b4c607c160ae
Brayanperez5 <163586839+Brayanperez5@users.noreply.github.com>
2024-03-15 16:06:55 -0500

    Add files via upload

---
+++ creacion numero aleatorio.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+using namespace std;
+main()
+{
+	srand(time(NULL));
+	int aleatorio=0; cout<<endl<<endl;
+	cout<<
+	"programa para generar 10 numeros aleatorios\n";
+	for(int i=1; i<11;i++)
+	{
+		aleatorio=rand()%7;
+		cout<<"el numero "<<i<<" generado es: "<<aleatorio<<endl; 
+	}
+	getch();
+}
+
+
+
